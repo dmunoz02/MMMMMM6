6bc8e6f77e9d538733320a10554c511366ce108f
dmunoz02 <106497393+dmunoz02@users.noreply.github.com>
2022-10-30 22:38:46 -0300

    Añado archivo

---
+++ Scrapingmaru.py
@@ -0,0 +1,6 @@
+#Codigo Xpatch en HTTML
+
+#$x('//tr//td/text()').map(x=> x.wholeText) (Extraer todos los datos de la tabla
+
+#$x('//tr//td [@class="destacado"]/text()').map(x=> x.wholeText) ( Extraer Puntos por Pilotos)
+
